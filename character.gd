d6198242a3c32700be64066d7ba8b083297b563d
Juanfran <juanfran.alcantara@kaleidos.net>
2016-12-20 11:24:00 +0100

    wip character

--- character.gd
+++ character.gd
@@ -1,27 +1,10 @@
-extends Node2D
+extends KinematicBody2D
 
-var walkspeed = 200
-var targetPosition
-var body
-var moving = false
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
 
 func _ready():
-	body = get_node("body")
-	set_fixed_process(true)
-
-func _fixed_process(delta):
-	if body.get_pos() == targetPosition:
-		moving = false
-	
-	if moving:
-		var direction = (targetPosition - body.get_pos()).normalized()
-		print(direction)
-		var motion = direction * walkspeed * delta
-		body.move(motion)
-
-func moveTo(_destination_):
-	targetPosition = _destination_
-	print(body.get_pos())
-	print(targetPosition)
-	print(targetPosition - body.get_pos())
-	#moving = true
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
