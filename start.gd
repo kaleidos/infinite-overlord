104613c778ac7d5de6399fbf3f28ca97589a2188
Tere <teresa.delatorre@kaleidos.net>
2016-12-23 10:39:13 +0100

    Add ESC to start page

--- start.gd
+++ start.gd
@@ -1,4 +1,11 @@
 extends Node2D
 
+func _ready():
+	set_process_input(true)
+
+func _input(event):
+	if (event.is_action_pressed("ui_cancel")):
+        self.get_tree().quit()
+
 func _on_StartButton_button_up():
 	get_tree().change_scene("res://main.tscn")
